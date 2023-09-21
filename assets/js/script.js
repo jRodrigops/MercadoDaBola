cc06ba11b86df4a22e8189a162146e7bd0254fb8
JOSE RODRIGO PORTELA SILVA <jose.rpsilva9@senacsp.edu.br>
2023-09-20 21:57:26 -0300

    codigo 2009 noite

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,22 @@ console.log(nome)
 const campoQtde = document.querySelector("#qtde")
 const output = document.querySelector("output")
 const campotipo = document.querySelector("#tipo")
+const campoLayoutSim = document.querySelector("#layout_sim")
+const campoLayoutNao = document.querySelector("#layout_nao")
+const campoJs = document.querySelector("#js")
+const campoPrazo = document.querySelector("#Prazo")
 
 campoQtde.addEventListener("change", calcular)
 campoQtde.addEventListener("change", calcular)
+campoLayoutSim.addEventListener("change", calcular)
+campoLayoutNao.addEventListener("change", calcular)
+campoJs.addEventListener("change", calcular)
+campoPrazo.addEventListener("input", function(){
+    const labelPrazo = document.querySelector("label[for='prazo']")
+    labelPrazo.innerHTML = `Prazo: ${campoPrazo.value} semanas`
+    calcular()
+})
+
 
 // calcular preço
 function calcular(){
@@ -15,9 +28,15 @@ function calcular(){
     let qtde = campoQtde.value 
     let valor = qtde * 100
    
-    if(campotipo.value == 2) {valor += 1000
-        
-    }
+    if(campotipo.value == 2) valor += 1000
+    if(campoLayoutSim.checked) valor += qtde * 50
+    
+    if(campoJs.checked) valor *= 1.1
+
+    let taxaDeUrgencia = 1 - campoPrazo.value * 0.05
+    valor *= 1 + taxaDeUrgencia
+
+
     console.log(tipo.value)
     output.innerText = `R$ ${valor}`
  
